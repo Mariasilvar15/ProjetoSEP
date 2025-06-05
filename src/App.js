de397706d8227337ab5bc88adc8a44e9149aee49
Mariasilvar15 <maria.silvar1511@gmail.com>
2025-06-05 20:04:11 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -100,8 +100,14 @@ function App() {
           <p>O sistema pode ser configurado para monitorar e ajustar a temperatura de maneira contínua, garantindo que ela se mantenha dentro dos limites ideais. O monitoramento em tempo real pode ser feito via monitor serial (para visualizar os valores no computador) ou através de uma interface com display LCD (para visualização no local).</p>
         </div>
         <div className='text-box' id='ultimo'>
-          <h3>Exibição e Ajuste</h3>
-          <p>O sistema pode ser configurado para monitorar e ajustar a temperatura de maneira contínua, garantindo que ela se mantenha dentro dos limites ideais. O monitoramento em tempo real pode ser feito via monitor serial (para visualizar os valores no computador) ou através de uma interface com display LCD (para visualização no local).</p>
+          <video muted autoPlay loop height="600">
+            <source src={`${process.env.PUBLIC_URL}/video2.mp4`} type="video/mp4" />
+            Seu navegador não suporta vídeo.
+          </video>  
+          <video muted autoPlay loop height="600">
+            <source src={`${process.env.PUBLIC_URL}/video1.mp4`} type="video/mp4" />
+            Seu navegador não suporta vídeo.
+          </video>
         </div>
       </section>
         
